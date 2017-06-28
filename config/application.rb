abcd08fa180018ed62a84a67d23f472f51e98070
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-06-28 13:50:09 -0700

    assignment-2-complete

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,5 +10,16 @@ module Blocipedia
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    ActionMailer::Base.smtp_settings = {
+      :address  => 'smtp.gmail.com',
+      :domain   => 'mail.google.com',
+      :port     => 587,
+      :user_name  => ENV["GMAIL_USERNAME"],
+      :password => ENV['GMAIL_PASSWORD'],
+      :authentication => 'login',
+      :enable_starttls_auto => true
+    }
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
